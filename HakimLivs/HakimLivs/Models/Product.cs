2df39bc45be7647e1574338c2a0f12a4030f4628
Patrik Carlsson <patrik.83.carlsson@gmail.com>
2022-04-28 16:38:58 +0200

    Update

---
+++ HakimLivs/HakimLivs/Models/Product.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HakimLivs.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Image { get; set; }
+        public double Price { get; set; }
+        public double? DiscountPrice { get; set; }
+        public double ComparisonPrice { get; set; }
+        [Required]
+        public string Origin { get; set; }
+        [Column(TypeName = "smalldatetime")]
+        public DateTime ExpirationDate { get; set; }
+        public int Stock { get; set; }
+        [Required]
+        public string UnitType { get; set; }
+        public double UnitValue { get; set; }
+    }
+}
