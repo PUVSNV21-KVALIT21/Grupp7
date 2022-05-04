d5c215ff95b77f78632e2938ecf9383838cd7f2a
Nils Landegren <nils.landegren@learnet.se>
2022-05-04 10:26:09 +0200

    added CRUD razor pages for Product

---
+++ HakimLivs/Pages/Products/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using HakimLivs.Data;
+using HakimLivs.Models;
+
+namespace HakimLivs.Pages.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly HakimLivs.Data.ApplicationDbContext _context;
+
+        public CreateModel(HakimLivs.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Product Product { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Products.Add(Product);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
