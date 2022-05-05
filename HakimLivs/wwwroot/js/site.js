000e0747a6a21d092d8d41ee3ef453165f8f313b
nils <n.landegren@hotmail.com>
2022-05-05 13:00:27 +0200

    added localStorage functionality for shopping cart

--- HakimLivs/wwwroot/js/site.js
+++ HakimLivs/wwwroot/js/site.js
@@ -2,3 +2,27 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function updateCartIcon() {
+    cartQuantity = localStorage.getItem('cartQuantity')
+    if (!cartQuantity) {
+        localStorage.setItem('cartQuantity', 0)
+    }
+    document.getElementById('cart-icon').textContent = cartQuantity
+}
+
+function addProductToLocalStorage(id) {
+    // {'productId': '<quantity>'}
+    currentProductQuantity = localStorage.getItem(id)
+    if (currentProductQuantity) {
+        localStorage.setItem(id, Number(currentProductQuantity) + 1)
+    }
+    else {
+        localStorage.setItem(id, 1)
+    }
+    newCartQuantity = Number(localStorage.getItem('cartQuantity')) + 1
+    localStorage.setItem('cartQuantity', newCartQuantity)
+
+    updateCartIcon()
+
+}
