a7f6b20648e511b286e44ea813c076d2b82d8f22
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2022-05-10 09:18:33 +0200

    Bugfix for discount prices not reading in (#100)
    * Bugfix/77 & 78 (#85)
    
    * Price with "," instead of "."
    
    * Also fix 78
    
    * Added method to display prices + Unit tests (#86)
    
    * Fixed some formatting and added helper methods (#88)
    
    * Fix/77unittests (#91)
    
    * Fixed some formatting and added helper methods
    
    * Update UnitTests.cs
    
    * Added left navigation meny with categories (#95)
    
    * Feature/71 (#96)
    
    * New Models Created Register Page Added
    
    * updated
    
    * IdentityOverriden and ProductModel changed
    
    Co-authored-by: Patrik Carlsson <patrik.83.carlsson@gmail.com>
    
    * Discount price working again (#98)
    
    Co-authored-by: Olagus84 <75989637+Olagus84@users.noreply.github.com>
    Co-authored-by: Patrik Carlsson <patrik.83.carlsson@gmail.com>

--- HakimLivs/Data/DbInitializer.cs
+++ HakimLivs/Data/DbInitializer.cs
@@ -75,7 +75,7 @@ namespace HakimLivs.Data
 
     public class DiscountPrice
     {
-        public string number { get; set; }
+        public Price price { get; set; }
     }
 
     public class Measurement
@@ -220,7 +220,6 @@ namespace HakimLivs.Data
                             if (data != null)
                             {
                                 var dataObj = JsonSerializer.Deserialize<HakimData>(data);
-                                Console.WriteLine($"Name: {dataObj.data[0].attributes.title}");
 
                                 foreach (var item in dataObj.data)
                                 {
