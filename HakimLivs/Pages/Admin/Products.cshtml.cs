2295c785c1a1d88a32e0dde31df67dbebbe7b43f
RikhardHonkanen <75729661+RikhardHonkanen@users.noreply.github.com>
2022-05-11 09:02:17 +0200

    Basic admin page/routing

---
+++ HakimLivs/Pages/Admin/Products.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HakimLivs.Pages.Admin
+{
+    public class ProductsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
