3c8b2c2de396822defd696b0f4ba847ea5bd1748
Olagus84 <75989637+Olagus84@users.noreply.github.com>
2022-05-11 16:24:11 +0200

    feature/39 Added Order Pages details/delete and Admin/Order

---
+++ HakimLivs/Migrations/20220511134816_discountCodeCanBeNull.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HakimLivs.Migrations
+{
+    public partial class discountCodeCanBeNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DiscountCode",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DiscountCode",
+                table: "Orders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
--- HakimLivs/Pages/Admin/Orders.cshtml.cs
+++ HakimLivs/Pages/Admin/Orders.cshtml.cs
@@ -1,12 +1,29 @@
+using HakimLivs.Data;
+using HakimLivs.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace HakimLivs.Pages.Admin
 {
     public class OrdersModel : PageModel
     {
-        public void OnGet()
+
+        private readonly ApplicationDbContext _context;
+        private readonly ILogger<IndexModel> _logger;
+
+        public OrdersModel(ILogger<IndexModel> logger, ApplicationDbContext context)
         {
+            _context = context;
+            _logger = logger;
+        }
+
+        public List<Order> Orders { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string? Message { get; set; }
+        public async Task OnGetAsync()
+        {
+            Orders = await _context.Orders.Include(o => o.User).ToListAsync();
         }
     }
 }
