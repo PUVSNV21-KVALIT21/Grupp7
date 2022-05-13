23cfedfd8d987def66ea8d6f276de4d19c722166
Philip Henningsson <75726309+philiphenningsson@users.noreply.github.com>
2022-05-13 11:35:26 +0200

    added customers section to admin panel (#124)

---
+++ HakimLivs/Pages/Admin/Customers.cshtml.cs
@@ -0,0 +1,36 @@
+using HakimLivs.Data;
+using HakimLivs.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+
+namespace HakimLivs.Pages.Admin
+{
+    public class CustomersModel : PageModel
+    {
+
+        private readonly ApplicationDbContext database;
+        private readonly UserManager<IdentityUser> _userManager;
+
+
+        public CustomersModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            database = context;
+            _userManager = userManager;
+        }
+
+        [BindProperty(SupportsGet = true)]
+        public string? Message { get; set; }
+        public AppUser? appUser { get; set; }
+        public List<AppUser> appUsers { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            appUsers = await database.Users.ToListAsync();
+            var httpUser = _userManager.GetUserAsync(User).Result;
+            appUser = await database.Users.FirstOrDefaultAsync(u => u.Id == httpUser.Id);
+        }
+    }
+}
