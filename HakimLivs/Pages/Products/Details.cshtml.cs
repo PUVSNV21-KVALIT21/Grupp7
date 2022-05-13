59b6eab03c5b39cd83cc15ccfde6756c9f311cba
PatrikCarlsson83 <75728054+PatrikCarlsson83@users.noreply.github.com>
2022-05-13 07:26:29 +0200

    Added connection to DB from details (#122)

--- HakimLivs/Pages/Products/Details.cshtml.cs
+++ HakimLivs/Pages/Products/Details.cshtml.cs
@@ -7,25 +7,34 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using HakimLivs.Data;
 using HakimLivs.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace HakimLivs.Pages.Products
 {
     public class DetailsModel : PageModel
     {
-        private readonly HakimLivs.Data.ApplicationDbContext _context;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public DetailsModel(HakimLivs.Data.ApplicationDbContext context)
+        public DetailsModel(ApplicationDbContext context, UserManager<IdentityUser> userManager)
         {
             _context = context;
+            _userManager = userManager;
         }
 
         public Product Product { get; set; }
+        public AppUser AppUser { get; set; }
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
             if (id == null)
             {
                 return NotFound();
+            }
+            var httpUser = _userManager.GetUserAsync(User).Result;
+            if (httpUser != null)
+            {
+                AppUser = await _context.Users.FirstOrDefaultAsync(u => u.Id == httpUser.Id);
             }
 
             Product = await _context.Products.FirstOrDefaultAsync(m => m.ID == id);
@@ -35,6 +44,21 @@ namespace HakimLivs.Pages.Products
                 return NotFound();
             }
             return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int id)
+        {
+            Product = await _context.Products.Include(a => a.AppUsers).SingleAsync(p => p.ID == id);
+
+            var httpUser = _userManager.GetUserAsync(User).Result;
+            var user = await _context.Users.Include(p => p.Products).SingleAsync(u => u.Id == httpUser.Id);
+
+            if (user != null)
+            {
+                user.Products.Add(Product);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage("../Index");
         }
     }
 }
